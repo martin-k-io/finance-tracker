17096a04ea6ef4f8f5ad233fb42318d36face0c5
martink-io <martinkiogit@gmail.com>
2017-11-06 13:23:06 +0000

    Add stocks to portfolio with restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
+
   devise_scope :user do
     post '/users/sign_out' => 'devise/sessions#destroy'
   end
